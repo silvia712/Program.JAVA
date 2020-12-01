dc04bb654c29c65b142442b40570608b5db284e2
silvia <sielma712@gmail.com>
2020-12-01 14:32:09 +0100

    revisando

--- src/Vectores/Ejer6_funciones.java
+++ src/Vectores/Ejer6_funciones.java
@@ -4,7 +4,16 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Ejer6_funciones {
+	
+	/*Crea una matriz de n x m donde n y m los leer�s por teclado. Crea un
+procedimiento (subproceso) que te rellene la matriz con n�meros aleatorios
+(recuerda la funci�n azar(X)). Crea otro procedimiento (subproceso) que se
+encargue de mostrar por pantalla la matriz, pero cada fila de la matriz la pintara
+en la misma l�nea. Para eso puedes usar la instrucci�n �Sin saltar�. Ejemplo de
+matriz 3x2
+	*/
 
+	
 	public static void aleatorio (int mTabla[][]) {
 		
 		Random azar = new Random();
